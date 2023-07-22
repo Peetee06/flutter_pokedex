f29bbea04deaad91589880eda7b3f1054f83ecf8
Peter Trost <trost@intervall.io>
2023-07-22 17:27:09 +0200

    Forcing error to test pipeline

--- lib/core/fade_page_route.dart
+++ lib/core/fade_page_route.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class FadeRoute extends PageRouteBuilder {
-  FadeRoute({required this.page})
       : super(
           pageBuilder: (_, __, ___) => page,
           transitionsBuilder: (_, animation, __, child) => FadeTransition(
